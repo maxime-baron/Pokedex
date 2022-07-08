8c0de28b77fa745257dfd3a072c3d2a1da849bc7
Ewixam <74783098+Ewixam@users.noreply.github.com>
2022-07-08 18:14:49 +0200

    :bug: Bug fix

--- includes/config.php
+++ includes/config.php
@@ -9,6 +9,7 @@ function apiCall($url)
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    // curl_setopt($curl, CURLOPT_CAINFO, $_SERVER['DOCUMENT_ROOT'] .  "/../ssl/cacert-2022-04-26.pem");
 
     $result = curl_exec($curl);
     curl_close($curl);
